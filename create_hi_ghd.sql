086c52274f61543816590ed2ed7021bd724bf8fd
smgopinath <160997325+smgopinath@users.noreply.github.com>
2024-03-03 13:13:22 +0530

    DB changes in SIT ,UAT and PERF
    Amol 3-Mar

---
+++ create_hi_ghd.sql
@@ -0,0 +1,29 @@
+drop table t_hi_ghd_ans;
+
+CREATE TABLE t_hi_ghd_ans (
+  ghdans_id bigint NOT NULL AUTO_INCREMENT,
+  ghddetails_id bigint DEFAULT NULL,
+  qn_id bigint DEFAULT NULL,
+  ans varchar(45) DEFAULT NULL,						   
+  PRIMARY KEY (ghdans_id),
+  KEY fk_ghd_dtls_idx (ghddetails_id),
+  KEY fk_ghd_ques_idx (qn_id),
+  CONSTRAINT fk_ghd_dtls FOREIGN KEY (ghddetails_id) REFERENCES t_hi_ghd_details (ghddetails_id),
+  CONSTRAINT fk_ghd_ques FOREIGN KEY (qn_id) REFERENCES t_hi_ghd_qstns (ghdqstn_id)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+drop table t_hi_ghd_details;
+
+CREATE TABLE t_hi_ghd_details (
+  ghddetails_id bigint NOT NULL AUTO_INCREMENT,
+  proposer_insured_id bigint DEFAULT NULL,
+  ghd_discount_recieved varchar(5) DEFAULT NULL,
+  ghd_member_discount double DEFAULT NULL,
+  ghd_aggregate_discount double DEFAULT NULL,								
+  active bit(1) DEFAULT NULL,
+  created_date datetime(6) DEFAULT NULL,
+  created_by varchar(45) DEFAULT NULL,
+  PRIMARY KEY (ghddetails_id),
+  KEY fk_proposer_insurance_id_idx (proposer_insured_id),
+  CONSTRAINT fk_prop_ind_id FOREIGN KEY (proposer_insured_id) REFERENCES t_hi_proposer_insured_members (proposerinsurerid)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Trans,HI Good Health Details from careplix';
